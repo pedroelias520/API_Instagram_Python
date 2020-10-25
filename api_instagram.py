0f1d1d949f2a93427a62f06feb1e8ff918ee5af3
Pedro Elias <epedro520@gmail.com>
2020-10-25 17:40:17 -0300

    upgrade: follow and unfollow created

--- api_instagram.py
+++ api_instagram.py
@@ -5,30 +5,32 @@ Created on Thu Oct 22 08:22:44 2020
 @author: Pedro Elias
 """
 import random as random
+from random import randint
 from selenium import webdriver
 from time import sleep
-from decimal import Decimal 
+from decimal import Decimal
+import time
+from progress import Bar
 
-def __init__():
-      logIn()
-      FollowPeople()
-      print('O algoritmo está em execução....')
+bar = Bar('Processing', max=50)
+browser =  webdriver.Firefox(executable_path='C:\geckodriver.exe')
+class main:
+    def __init__(self): 
+        print('O algoritmo está em execução....')
+        logIn()
+        FollowPeople()
+        UnfllowPeople()
 
-def logIn():
-    browser =  webdriver.Firefox()
-    browser.implicitly_wait(5)
+def logIn():       
     browser.get('https://www.instagram.com/accounts/login/')
-
-    login_link = browser.find_element_by_xpath("//a[text()='Log in']")
-    login_link.click()
-
+    
     sleep(2)
 
     username_input = browser.find_element_by_css_selector("input[name='username']")
     password_input = browser.find_element_by_css_selector("input[name='password']")
 
-    username_input.send_keys("@pedro_sousa_figueredo")
-    password_input.send_keys("drawepic16")
+    username_input.send_keys("@eu_pedro_sousa")
+    password_input.send_keys("drawepic19")
 
     login_button = browser.find_element_by_xpath("//button[@type='submit']")
     try:
@@ -36,24 +38,63 @@ def logIn():
     except:
         print('Erro no login')
     sleep(5)
+   
+def UnfllowPeople():
+    browser.get('https://www.instagram.com/pedro_sousa_figueredo/')
+    profile_logo_following = browser.find_element_by_xpath("//span[@class='g47SY']")
+    profile_logo_following.click()
+    number_of_random = randint(0, 20)
+    cont_num = 0
+    following_button = browser.find_element_by_xpath("//button[text()='Seguindo']")
+    for i in following_button:
+        if(cont_num<=number_of_random):
+            i.click()
+            cont_num=cont_num + 1
+            bar.next()
+        else:
+            print("Target Acquired")
+    bar.finish()
     
-def FollowPeople():   
-    browser =  webdriver.Firefox()
+def FollowPeople():       
+    not_now = browser.find_element_by_xpath("//button[text()='Agora não']")
+    not_now.click()
+    browser.implicitly_wait(2)
+    not_now = browser.find_element_by_xpath("//button[text()='Agora não']")
+    not_now.click()
+    
     browser.implicitly_wait(5)
-    see_all = browser.find_element_by_css_selector("div[class ='_7UhW9 PIoXz qyrsm KV-D4 uL8Hv ']")
-    peoples = []
-    infinite = Decimal('Infinity')
-    
+    see_all = browser.find_element_by_xpath("//a[@href ='/explore/people/']")
+    see_all.click()
+    browser.implicitly_wait(2)    
+    infinite = 1
+    cont = 0    
     #Get all people to follow
     
-    for i in infinite:        
-        number_of_people = random(50)
-        for i in number_of_people:
-            people_list = browser.find_element_by_css_selector("div[class = 'Igw0E rBNOH eGOV_ ybXk5 _4EzTm XfCBB HVWg4 ']")
-            peoples.append(people_list)
-        print(peoples)
-        sleep(3600)
+    while(infinite == 1):
+        number_of_people = randint(0,50)
+        print('Number of repetitions')
+        print(number_of_people)
+        print('=========================')
+        #Colect all id's od person
+        
+        person = browser.find_elements_by_xpath("//button[text()='Seguir']")           
+        if (person == ''):
+            print('Users not founded')
+            break
+        
+        print('Pessoas')
+        print(person)
+        print('=========================')                                
+        #Follow all person's    
+        for i in person:
+            if (cont<=number_of_people):
+                bar.next()                     
+                i.click()   
+                cont = cont + 1
+            else:                
+                print('Target acquired')
+                break
     
-    
+main()
         
     
